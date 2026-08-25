efcaad3c0accffd135f8b817dba99f37f5bd3ce2
Bhavya Nigam <bhavya.nigam@joshtechnologygroup.com>
2026-08-25 21:47:54 +0530

    [BN_A2_02]: Fix search route

--- src/routes/RoutePath.tsx
+++ src/routes/RoutePath.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 
 import { Login } from '@pages/Login';
 import { Profile } from '@pages/Profile';
+import { Search } from '@pages/Search';
 
 export const RoutePath = () => (
     <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Search />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/search" element={<Search />}></Route>
     </Routes>
 );
